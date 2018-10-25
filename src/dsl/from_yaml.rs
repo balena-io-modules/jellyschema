2840fb3e5e7759d912eed5e9c6c711c1deaf0372
Cyryl Płotnicki <cyryl@balena.io>
2018-10-25 12:07:04 +0100

    slight refactoring of yaml schema deserializer

--- src/dsl/from_yaml.rs
+++ src/dsl/from_yaml.rs
@@ -1,56 +1,59 @@
+use crate::dsl::compiler::Property;
 use crate::dsl::compiler::PropertyEntry;
 use crate::dsl::compiler::PropertyList;
+use serde::de::Error;
 use serde::{Deserialize, Deserializer};
+use serde_yaml::Mapping;
+use serde_yaml::Sequence;
 
 pub fn deserialize_property_list<'de, D>(deserializer: D) -> Result<Option<PropertyList>, D::Error>
 where
     D: Deserializer<'de>,
 {
     let maybe_sequence: Option<serde_yaml::Sequence> = Option::deserialize(deserializer)?;
-    let mut property_names = vec![];
     match maybe_sequence {
-        Some(sequence) => {
-            let list_of_maybe_entries = sequence.iter().map(|value| match value.as_mapping() {
-                Some(mapping) => {
-                    let (key, value) = match mapping.into_iter().next() {
-                        Some(s) => s,
-                        None => return Err(serde::de::Error::custom("cannot get first element of the sequence")),
-                    };
-                    let key: String = match serde_yaml::from_value(key.clone()) {
-                        Ok(k) => k,
-                        Err(e) => {
-                            return Err(serde::de::Error::custom(format!(
-                                "cannot deserialize the key - {:?}",
-                                e
-                            )))
-                        }
-                    };
-                    let value = match serde_yaml::from_value(value.clone()) {
-                        Ok(k) => k,
-                        Err(e) => {
-                            return Err(serde::de::Error::custom(format!(
-                                "cannot deserialize the value {:?}",
-                                e
-                            )))
-                        }
-                    };
-                    property_names.push(key.clone());
-                    Ok(PropertyEntry {
-                        name: key,
-                        property: value,
-                    })
-                }
-                None => Err(serde::de::Error::custom("")),
-            });
-
-            let list: Result<Vec<_>, D::Error> = list_of_maybe_entries.collect();
-            let list = list?;
-
-            Ok(Some(PropertyList {
-                entries: list,
-                property_names: property_names.clone(),
-            }))
-        }
+        Some(sequence) => Ok(Some(sequence_to_property_list(sequence)?)),
         None => Ok(None),
     }
+}
+
+fn sequence_to_property_list<E>(sequence: Sequence) -> Result<PropertyList, E>
+where
+    E: Error,
+{
+    let mut property_names = vec![];
+    let list_of_maybe_entries = sequence.into_iter().map(|value| {
+        let mapping = value
+            .as_mapping()
+            .ok_or(Error::custom("cannot deserialize property as mapping"))?;
+        let property_entry = mapping_to_property_entry(mapping)?;
+        property_names.push(property_entry.name.clone());
+        Ok(property_entry)
+    });
+
+    let list: Result<Vec<_>, E> = list_of_maybe_entries.collect();
+    let list = list?;
+
+    Ok(PropertyList {
+        entries: list,
+        property_names: property_names.clone(),
+    })
+}
+
+fn mapping_to_property_entry<E>(mapping: &Mapping) -> Result<PropertyEntry, E>
+where
+    E: Error,
+{
+    let (key, value) = mapping
+        .into_iter()
+        .next()
+        .ok_or(Error::custom("cannot get first element of the sequence"))?;
+    let key: String =
+        serde_yaml::from_value(key.clone()).map_err(|e| Error::custom(format!("cannot deserialize key - {:?}", e)))?;
+    let value: Property = serde_yaml::from_value(value.clone())
+        .map_err(|e| Error::custom(format!("cannot deserialize value - {:?}", e)))?;
+    Ok(PropertyEntry {
+        name: key,
+        property: value,
+    })
 }
