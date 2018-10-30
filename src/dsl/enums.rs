57b2fccded5a08978a03a3e6420f8aa086142ca3
Cyryl Płotnicki <cyryl@balena.io>
2018-10-30 11:32:27 +0000

    split out conversion from mapping to enumeration value

--- src/dsl/enums.rs
+++ src/dsl/enums.rs
@@ -4,6 +4,7 @@ use crate::dsl::schema::DisplayInformation;
 use serde::de::Error;
 use serde::Deserialize;
 use serde::Deserializer;
+use serde_yaml::Mapping;
 use serde_yaml::Value;
 
 #[derive(Clone, Debug)]
@@ -25,62 +26,76 @@ impl<'de> Deserialize<'de> for EnumerationValues {
     {
         let definitions: Vec<Value> = Vec::deserialize(deserializer)
             .map_err(|e| Error::custom(format!("cannot deserialize sequence - {}", e)))?;
-        let mut enumeration_values = vec![];
-        for definition in definitions {
-            let enumeration_value = {
-                if definition.is_string() {
-                    let title = definition
-                        .as_str()
-                        .expect("unwrapping as string failed - serde_yaml inconsistency")
-                        .to_string();
-                    let display_information = DisplayInformation {
-                        title: Some(title.clone()),
-                        help: None,
-                        warning: None,
-                        description: None,
-                    };
-                    EnumerationValue {
-                        display_information,
-                        value: title.clone(),
-                        type_spec: ObjectType::Required(RawObjectType::String),
+        let enumeration_values: Result<Vec<EnumerationValue>, D::Error> = definitions
+            .iter()
+            .map(|definition| {
+                Ok({
+                    if definition.is_string() {
+                        let title = definition
+                            .as_str()
+                            .expect("unwrapping as string failed - serde_yaml inconsistency");
+                        title.into()
+                    } else if definition.is_mapping() {
+                        let mapping = definition
+                            .as_mapping()
+                            .expect("unwrapping mapping failed - serde_yaml inconsistency");
+                        mapping_to_enumeration_value(mapping)?
+                    } else {
+                        return Err(Error::custom(format!("no idea how to deserialize {:?}", definition)));
                     }
-                } else if definition.is_mapping() {
-                    // value or title
-                    let mapping = definition
-                        .as_mapping()
-                        .expect("unwrapping mapping failed - serde_yaml inconsistency");
-                    let mut value = mapping.get(&Value::from("value"));
-                    let mut title = mapping.get(&Value::from("title"));
-                    if title.is_none() {
-                        title = value;
-                    }
-                    if value.is_none() {
-                        value = title;
-                    }
-                    if value.is_none() {
-                        return Err(Error::custom("no value for enum"));
-                    }
-                    let value = value.unwrap().as_str().unwrap().to_string();
-                    let title = title.unwrap().as_str().unwrap().to_string();
-                    let display_information = DisplayInformation {
-                        title: Some(title),
-                        help: None,
-                        warning: None,
-                        description: None,
-                    };
-                    EnumerationValue {
-                        display_information,
-                        value,
-                        type_spec: ObjectType::Required(RawObjectType::String),
-                    }
-                } else {
-                    return Err(Error::custom(format!("no idea how to deserialize {:?}", definition)));
-                }
-            };
-            enumeration_values.push(enumeration_value);
-        }
+                })
+            })
+            .collect();
+
         Ok(EnumerationValues {
-            possible_values: enumeration_values,
+            possible_values: enumeration_values?,
         })
     }
+}
+
+impl From<&str> for EnumerationValue {
+    fn from(value: &str) -> Self {
+        let title = value.to_string();
+        let display_information = DisplayInformation {
+            title: Some(title.clone()),
+            help: None,
+            warning: None,
+            description: None,
+        };
+        EnumerationValue {
+            display_information,
+            value: title.clone(),
+            type_spec: ObjectType::Required(RawObjectType::String),
+        }
+    }
+}
+
+fn mapping_to_enumeration_value<E>(mapping: &Mapping) -> Result<EnumerationValue, E>
+where
+    E: Error,
+{
+    let mut value = mapping.get(&Value::from("value"));
+    let mut title = mapping.get(&Value::from("title"));
+    if title.is_none() {
+        title = value;
+    }
+    if value.is_none() {
+        value = title;
+    }
+    if value.is_none() {
+        return Err(Error::custom("no value for enum"));
+    }
+    let value = value.unwrap().as_str().unwrap().to_string();
+    let title = title.unwrap().as_str().unwrap().to_string();
+    let display_information = DisplayInformation {
+        title: Some(title),
+        help: None,
+        warning: None,
+        description: None,
+    };
+    Ok(EnumerationValue {
+        display_information,
+        value,
+        type_spec: ObjectType::Required(RawObjectType::String),
+    })
 }
