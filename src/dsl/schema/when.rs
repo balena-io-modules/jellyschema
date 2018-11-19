073f1254d29363de958bb1605944ad70f7f73786
Cyryl Płotnicki <cyryl@balena.io>
2018-11-19 17:18:02 +0000

    Small documentation for the when parsing

--- src/dsl/schema/when.rs
+++ src/dsl/schema/when.rs
@@ -1,3 +1,4 @@
+//! This module is responsible for parsing the `when` keyword - creating a dependency tree between Schemas
 use std::collections::HashMap;
 
 use balena_temen::ast::Expression;
@@ -8,11 +9,42 @@ use balena_temen::ast::IdentifierValue;
 use crate::dsl::schema::compiler::CompilationError;
 use crate::dsl::schema::SchemaList;
 
+/// This represents the whole dependency graph of the whole yaml schema document
 // TODO: use actual DAG for representing the whole graph of dependencies between the schemas
 #[derive(Debug, Clone)]
 pub struct DependencyGraph {
     // schema name -> its dependencies
     all: HashMap<String, DependencyTree>,
+}
+
+/// This is all dependencies of a particular Schema, represented as names of other Schemas
+#[derive(Debug, Clone)]
+struct DependencyTree {
+    tree: Vec<String>, // TODO: change into actual tree
+}
+
+pub fn dependencies_for_schema_list(
+    maybe_list: Option<&SchemaList>,
+    previous_tree: DependencyGraph,
+) -> Result<DependencyGraph, CompilationError> {
+    match maybe_list {
+        None => Ok(DependencyGraph::empty()),
+        Some(list) => {
+            let mut tree = previous_tree;
+            for schema in list.entries() {
+                if let Some(when) = &schema.schema.when {
+                    tree = tree.push(&schema.name, &when);
+                }
+
+                if let Some(children) = &schema.schema.children {
+                    for named_child in children.entries() {
+                        tree = dependencies_for_schema_list(named_child.schema.children.as_ref(), tree)?;
+                    }
+                }
+            }
+            Ok(tree)
+        }
+    }
 }
 
 impl DependencyGraph {
@@ -27,27 +59,6 @@ impl DependencyGraph {
             vec![]
         }
     }
-}
-
-#[derive(Debug, Clone)]
-struct DependencyTree {
-    tree: Vec<String>, // TODO: change into actual tree
-}
-
-impl DependencyTree {
-    fn start_with(identifiers: &Identifier) -> DependencyTree {
-        let mut result = vec![];
-        for identifier in &identifiers.values {
-            match identifier {
-                IdentifierValue::Name(name) => {
-                    result.push(name.clone());
-                }
-                _ => unimplemented!(),
-            }
-        }
-        DependencyTree { tree: result }
-    }
-    // TODO: see if we need `merge_with` as well
 }
 
 impl DependencyGraph {
@@ -79,26 +90,18 @@ impl DependencyGraph {
     }
 }
 
-pub fn dependencies_for_schema_list(
-    maybe_list: Option<&SchemaList>,
-    previous_tree: DependencyGraph,
-) -> Result<DependencyGraph, CompilationError> {
-    match maybe_list {
-        None => Ok(DependencyGraph::empty()),
-        Some(list) => {
-            let mut tree = previous_tree;
-            for schema in list.entries() {
-                if let Some(when) = &schema.schema.when {
-                    tree = tree.push(&schema.name, &when);
+impl DependencyTree {
+    fn start_with(identifiers: &Identifier) -> DependencyTree {
+        let mut result = vec![];
+        for identifier in &identifiers.values {
+            match identifier {
+                IdentifierValue::Name(name) => {
+                    result.push(name.clone());
                 }
-
-                if let Some(children) = &schema.schema.children {
-                    for named_child in children.entries() {
-                        tree = dependencies_for_schema_list(named_child.schema.children.as_ref(), tree)?;
-                    }
-                }
+                _ => unimplemented!(),
             }
-            Ok(tree)
         }
+        DependencyTree { tree: result }
     }
+    // TODO: see if we need `merge_with` as well
 }
