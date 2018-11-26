89332d1a3f821c605bbebbb1ac84d397a3bd6db7
Cyryl Płotnicki <cyryl@balena.io>
2018-11-26 10:51:41 +0000

    Add node.js as dependency
    Change-type: patch

--- ci/install.sh
+++ ci/install.sh
@@ -3,9 +3,31 @@
 set -e
 set -o pipefail
 
-rustup default `cat rust-toolchain`
+echo "Installing Rust toolchain..."
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain $(cat rust-toolchain)
+source "${HOME}/.cargo/env"
 rustup component add clippy-preview
 rustup component add rustfmt-preview
-pip install --user -r requirements.txt
-npm install -g ajv-cli
 
+echo "Updating Rust toolchain..."
+(test -x "${HOME}/.cargo/bin/cargo-install-update" || cargo install cargo-update)
+cargo install-update -a
+
+echo "Installing NVM & NodeJS..."
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+source "${HOME}/.nvm/nvm.sh"
+nvm install
+nvm use
+echo "NodeJS version $(node --version)"
+
+echo "Installing jest..."
+npm install -g jest
+
+echo "Installing wasm-pack..."
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -f
+
+echo "Installing yaml linter..."
+pip install --user -r requirements.txt
+
+echo "Installing JSONSchema linter..."
+npm install -g ajv-cli
