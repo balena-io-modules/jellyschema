c906faf4e89cae671f0292bb35c377cec23044ef
Robert Vojta <robert@balena.io>
2018-11-28 13:52:09 +0100

    Update docstrings
    Signed-off-by: Robert Vojta <robert@balena.io>

--- src/dsl/mod.rs
+++ src/dsl/mod.rs
@@ -1,3 +1,2 @@
-//! This contains everything concerning parsing the DSL from yaml into `CompiledSchema` understood by `output::Generator`
-//! Contains types representing DSL schema constructs and deserializers for them. Uses `serde_yaml` internally.
+//! A module containing the configuration DSL parser
 pub mod schema;
