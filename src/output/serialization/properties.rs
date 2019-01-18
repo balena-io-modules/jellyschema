50e0206fb4a73d34a2a42ca35ce0c454ec6e2300
Cyryl Płotnicki <cyryl@balena.io>
2019-01-18 15:16:42 +0000

    Remove support for compund types completely
    It turns out that these are confusing and implementation is error-prone.
    Change the underlying Rust type to not allow a `Vec<ObjectType>` but just
    `ObjectType` and basically relied on the compiler to tell me what to
    fix.
    
    Checked that the array type cases are unaffected - see tests in
    `tests/data/arrays`.
    
    Change-type: minor

--- src/output/serialization/properties.rs
+++ src/output/serialization/properties.rs
@@ -1,10 +1,8 @@
 use serde::ser::{Error, Serialize, SerializeMap, Serializer};
 
-use crate::dsl::schema::object_types::ObjectType;
 use crate::dsl::schema::Schema;
 use crate::dsl::schema::SchemaList;
 use crate::dsl::schema::when::DependencyGraph;
-use crate::output::serialization::object_types::object_type_name;
 use crate::output::serialization::object_types::serialize_object_type;
 use crate::output::serialization::when::serialize_schema_list_dependencies;
 
@@ -60,13 +58,8 @@ where
         map.serialize_entry("title", &title)?;
     }
 
-    if let Some(types) = &schema.types {
-        if types.len() == 1 {
-            serialize_object_type(&types[0], map)?;
-        }
-        if types.len() > 1 {
-            serialize_type_array(types, map)?;
-        }
+    if let Some(object_type) = &schema.object_type {
+        serialize_object_type(&object_type, map)?;
     }
 
     if let Some(children) = &schema.children {
@@ -80,19 +73,5 @@ where
     if let Some(formula) = &schema.formula {
         map.serialize_entry("$$formula", &formula)?;
     }
-    Ok(())
-}
-
-fn serialize_type_array<O, E, S>(types: &[ObjectType], map: &mut S) -> Result<(), E>
-where
-    E: Error,
-    S: SerializeMap<Ok = O, Error = E>,
-{
-    if types.iter().any(|def| def.inner_raw().has_bounds()) {
-        return Err(Error::custom("cannot have type bounds when having a multi-type object"));
-    }
-
-    let type_names: Vec<_> = types.iter().map(|def| object_type_name(def.inner_raw())).collect();
-    map.serialize_entry("type", &type_names)?;
     Ok(())
 }
