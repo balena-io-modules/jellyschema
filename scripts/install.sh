d217d1b04d826b6b5384664b7743494b4dfea4a7
Cyryl Płotnicki <cyryl@balena.io>
2019-02-08 11:12:19 +0000

    Fix omitted variable subsititution

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 RUSTUP_COMMAND="curl https://sh.rustup.rs -sSf | sh -s -- -y"
-if [ ! -z $CI ]; then
+if [ ! -z "${CI}" ]; then
     default_compiler=`cat rust-toolchain`
     echo "Setting the default compiler to ${default_compiler}"
     RUSTUP_COMMAND="$RUSTUP_COMMAND --default-toolchain ${default_compiler}"
